4622d126550d4893d7994eb159b78f9c759cf666
V Kumar <vikash2060t@gmail.com>
2023-08-24 18:31:01 +0530

    Update home.cy.js

--- cypress/e2e/Home/home.cy.js
+++ cypress/e2e/Home/home.cy.js
@@ -8,6 +8,7 @@ describe("Home page test", () => {
     beforeEach(() => {
       cy.visit("/");
       })
+      
     it("Check the redirection when click on Logo link", () => {
         homepage.clickLogo();
         homepage.verifyLogoRedirect();
