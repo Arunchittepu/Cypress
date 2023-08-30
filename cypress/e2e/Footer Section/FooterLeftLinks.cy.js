29ed6e668b1e81c35a4e7aa190cd68ae8a46a40e
V Kumar <vikash2060t@gmail.com>
2023-08-30 11:34:07 +0530

    Footer links

---
+++ cypress/e2e/Footer Section/FooterLeftLinks.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress"/>
+import homepage from "../../pageobjects/homepage";
+import footerleft from "../../pageobjects/FooterLeft";
+describe("Footer Left links test", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      })
+    it("Check the redirection when click on Logo link", () => {
+        homepage.clickLogo();
+        homepage.verifyLogoRedirect();
+      })
+
+      it.only("Check Customer Service link at footer left section", () => {
+        footerleft.assertCustomerServicelink();
+        footerleft.clickCustomerServicelink();
+        footerleft.assertCustomerSupportUrl();
+        footerleft.assertReadourFAQsHead();
+        footerleft.testReadOurFAQsSection();
+
+       
+      })
+
+});
+
+
+
+
